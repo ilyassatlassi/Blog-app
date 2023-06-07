5599c9b83af8b1c5229fd2808980dd79b4a38c87
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-07 19:19:57 +0100

    Create table user

--- db/migrate/20230607164537_create_users.rb
+++ db/migrate/20230607164537_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.text :bio
+      t.integer :posts_counter, default: 0
 
       t.timestamps
     end
