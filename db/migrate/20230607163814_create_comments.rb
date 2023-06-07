b1639c0ff5a4d0ce9e11cfd60f0bcfa93fe65591
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-07 19:28:03 +0100

    Create table comment

--- db/migrate/20230607163814_create_comments.rb
+++ db/migrate/20230607163814_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
+      t.text :text
+
+      t.references :author_id, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
