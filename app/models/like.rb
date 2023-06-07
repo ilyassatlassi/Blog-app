6b64361add995550b9d82f718133bb09c8a4290a
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-07 22:08:11 +0100

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    has_many :likes
-    has_many :comments
-    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes
+  has_many :comments
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
 end
