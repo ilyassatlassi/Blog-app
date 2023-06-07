240afefc50997b381ae7460c3b6aa37ffee26eeb
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-07 20:59:30 +0100

    Add rchange for author-id

--- db/migrate/20230607194035_revert_create_likes.rb
+++ db/migrate/20230607194035_revert_create_likes.rb
@@ -1,8 +1,6 @@
-class CreateComments < ActiveRecord::Migration[7.0]
+class RevertCreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
-      t.text :text
-
+    create_table :likes do |t|
       t.references :author, foreign_key: { to_table: :users }
       t.references :post, foreign_key: true
 
