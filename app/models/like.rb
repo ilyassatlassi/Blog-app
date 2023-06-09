679ad9ae306ab5403727d8d6d770b8f4a1149f07
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-09 21:36:28 +0100

    Add method to classes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,14 @@
 class Like < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     belongs_to :post
-  end
+
+    after_create :update_post_likes_counter
+    after_destroy :update_post_likes_counter
+
+    private
+
+    def update_post_likes_counter
+        post.update(likes_counter: post.likes.count)
+    end
+end
   
