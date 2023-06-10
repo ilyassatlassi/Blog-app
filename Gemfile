0ca8eefecb4f87e669f205448ca7c6da43ae0e06
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-10 16:01:52 +0100

    Add validation Post

--- Gemfile
+++ Gemfile
@@ -72,3 +72,12 @@ group :test do
 end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
+
+group :development, :test do
+  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   after_create :update_user_posts_counter
   after_destroy :update_user_posts_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
