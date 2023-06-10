85c90c0bae87c01466c3d26c15b611ea263b21bb
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-10 17:12:16 +0100

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,8 +5,6 @@ class Comment < ApplicationRecord
   after_create :update_comments_counter
   after_destroy :update_comments_counter
 
-
-
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
