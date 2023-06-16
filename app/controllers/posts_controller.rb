4987b39ae59456d969ff9f4a7f56ca19f989c7ff
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-16 21:52:35 +0100

    Add comment controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,28 @@ class PostsController < ApplicationController
   def show
     @post = @user.posts.find(params[:id])
   end
+  
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(current_user)
+
+    else
+      flash[:alert] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
 
   private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
   def fetch_author
     @user = User.find_by(id: params[:user_id])
