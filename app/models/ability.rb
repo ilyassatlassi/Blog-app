dac03fe22ae6d1d10f63806a5c835c5b3ed00979
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-22 21:22:29 +0100

    Generate devise user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,21 +1,2 @@
 # frozen_string_literal: true
 
-class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-  
-    can :read, :all
-
-    return unless user.present?
-
-    can :manage, User, id: user.id
-    can :manage, Post, author_id: user.id
-    can :manage, Comment, author_id: user.id
-    can :create, Like
-
-    return unless user.role == 'admin'
-
-    can :manage, :all
-  end
-end
